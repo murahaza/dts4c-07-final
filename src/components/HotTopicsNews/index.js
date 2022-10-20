776c84215216a2366da99e5ea0ef4300fbcf3de4
yuslianto <yuslianto56@gmail.com>
2022-10-20 21:31:54 +0700

    set color font

--- src/components/HotTopicsNews/index.js
+++ src/components/HotTopicsNews/index.js
@@ -11,19 +11,20 @@ const HotTopicsNews = () => {
       </Typography>
       <Box sx={{ display: 'flex', margin: 2 }} >
 
-        <Card sx={{ dispaly: 'flex', maxWidth: 1000, bgcolor: 'green' }}>
+        <Card sx={{ dispaly: 'flex', maxWidth: 1000 }}>
           <Box square={true} sx={{ display: 'flex', flexDirection: 'column' }}>
-            <CardActionArea sx={{ width: '100%', height: '100%', bgcolor: 'yellow' }}>
+            <CardActionArea sx={{ width: '100%', height: '100%' }}>
               <CardMedia
                 component='img'
                 image={banner}
                 alt='banner picture'
                 width={'100%'}
                 height={'100%'}
-                sx={{ bgcolor: 'yellow' }}
               />
-              <div style={{ zIndex: 100, position: 'absolute', color: 'white', left: 0,  bottom: 0, width: 350, padding: 10 }}>
-                <Typography fontSize={30} align='left'>Massa tortor nibh nulla <br /> condimentum imperdiet <br />scelerisque...</Typography>
+              <div style={{ zIndex: 100, position: 'absolute', left: 0, bottom: 0, width: 350, padding: 10 }}>
+                <Typography fontSize={30} align='left' color={theme.palette.primary.light}>
+                  Massa tortor nibh nulla <br /> condimentum imperdiet <br />scelerisque...
+                </Typography>
                 <Box sx={{ width: 160, padding: 0, margin: 0, justifyContent: 'space-between', display: 'flex', mt: 2 }}>
                   <Typography variant="caption" color={theme.palette.primary.main} fontSize={12}>
                     2 Haour Ago
@@ -55,7 +56,7 @@ const HotTopicsNews = () => {
 
 
         <Box sx={{ width: 270, ml: 2, mt: 1, display: { xs: 'none', md: 'flex' } }} align='left' >
-          <Typography>
+          <Typography variant="caption" color={theme.palette.primary.dark} fontSize={18}>
             Nisi, sagittis aliquet sit rutrum. Nunc, id vestibulum quam ornare adipiscing. Pellentesque sed turpis nunc gravida pharetra, sit nec vivamus pharetra. Velit, dui, egestas nisi, elementum mattis mauris, magnis. Massa tortor nibh nulla condimentum imperdiet scelerisque... read more
           </Typography>
         </Box>
